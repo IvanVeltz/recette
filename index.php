166bb7af912bc5a9f57ebd16fe85a16df160530d
Ivan Veltz <ivan.veltz@live.fr>
2025-03-27 14:58:49 +0100

    Debut PDO

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recette</title>
+</head>
+<body>
+    <?php
+    try{
+        $mysqlClient = new PDO(
+            'mysql:host=localhost;dbname=recette;charset=utf8',
+            'root',
+            ''
+        );
+    }
+    catch(Exception $e){
+        die('Erreur : '.$e->getMessage());
+    }
+
+    $sqlQuery = 'SELECT * FROM recette';
+    $recipesStatement = $mysqlClient->prepare($sqlQuery);
+    $recipesStatement->execute();
+    $recipes = $recipesStatement->fetchAll();
+
+    foreach ($recipes as $recipe){
+    ?>
+        <p><?php echo $recipe['nom_recette'] ?></p>
+    <?php
+    }
+    
+
+    ?>
+</body>
+</html>
